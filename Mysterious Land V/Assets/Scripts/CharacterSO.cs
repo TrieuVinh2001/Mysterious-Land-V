538a54049bf3a17a6b3ba50b0c236bc2dcc93e8e
Vinh Triệu Văn <trieuvanvinh2001@gmail.com>
2024-05-21 20:59:07 +0700

    Add Asset and script base

---
+++ Mysterious Land V/Assets/Scripts/CharacterSO.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Character", menuName = "Character")]
+public class CharacterSO : ScriptableObject
+{
+    public enum CharacterType { Player, enemy}
+
+    [Header("Attributes")]
+    public new string name;
+    public int id;
+    public CharacterType type;
+    public Sprite image;
+    public RuntimeAnimatorController anim;
+    public float coin;
+    public float hp;
+    public float damage;
+    public float armor;
+    public float moveSpeed;
+    public float attackSpeed;
+    public float attackRange;
+    public float critical;//Chí mạng
+    public float bloodSucker;//Hút máu
+    public float armorPiercing;//Xuyên giáp
+    public float healing;//Hồi máu
+
+}
